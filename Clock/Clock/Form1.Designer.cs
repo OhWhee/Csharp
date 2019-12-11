5d889f8a658329759a425914a8dc56a65bcce89f
OhWhee <42094630+OhWhee@users.noreply.github.com>
2019-12-11 19:14:11 +0300

    commit

---
+++ Clock/Clock/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dialBox = new System.Windows.Forms.PictureBox();
+            this.hrBox = new System.Windows.Forms.PictureBox();
+            this.minBox = new System.Windows.Forms.PictureBox();
+            this.secBox = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dialBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hrBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dialBox
+            // 
+            this.dialBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dialBox.BackColor = System.Drawing.Color.Transparent;
+            this.dialBox.Location = new System.Drawing.Point(12, 12);
+            this.dialBox.Name = "dialBox";
+            this.dialBox.Size = new System.Drawing.Size(947, 1000);
+            this.dialBox.TabIndex = 0;
+            this.dialBox.TabStop = false;
+            // 
+            // hrBox
+            // 
+            this.hrBox.BackColor = System.Drawing.Color.Transparent;
+            this.hrBox.Location = new System.Drawing.Point(12, 12);
+            this.hrBox.Name = "hrBox";
+            this.hrBox.Size = new System.Drawing.Size(800, 800);
+            this.hrBox.TabIndex = 1;
+            this.hrBox.TabStop = false;
+            // 
+            // minBox
+            // 
+            this.minBox.BackColor = System.Drawing.Color.Transparent;
+            this.minBox.Location = new System.Drawing.Point(12, 12);
+            this.minBox.Name = "minBox";
+            this.minBox.Size = new System.Drawing.Size(800, 800);
+            this.minBox.TabIndex = 2;
+            this.minBox.TabStop = false;
+            // 
+            // secBox
+            // 
+            this.secBox.BackColor = System.Drawing.Color.Transparent;
+            this.secBox.Location = new System.Drawing.Point(12, 12);
+            this.secBox.Name = "secBox";
+            this.secBox.Size = new System.Drawing.Size(800, 800);
+            this.secBox.TabIndex = 3;
+            this.secBox.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Location = new System.Drawing.Point(818, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(250, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 961);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.minBox);
+            this.Controls.Add(this.hrBox);
+            this.Controls.Add(this.dialBox);
+            this.Controls.Add(this.secBox);
+            this.Name = "Form1";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dialBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hrBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox dialBox;
+        private System.Windows.Forms.PictureBox hrBox;
+        private System.Windows.Forms.PictureBox minBox;
+        private System.Windows.Forms.PictureBox secBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Clock/Clock/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap dialImg, hourImg, minuteImg, secondImg;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dialImg = new Bitmap(Properties.Resources.dial);
+            hourImg = new Bitmap(Properties.Resources.hr);
+            minuteImg = new Bitmap(Properties.Resources.min);
+            secondImg = new Bitmap(Properties.Resources.sec);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime currentTimeUTC = DateTime.UtcNow;
+            
+            int minute = currentTimeUTC.Minute + (int)timeConverter(currentTimeUTC)[1];
+            int second = currentTimeUTC.Second + (int)timeConverter(currentTimeUTC)[2];
+            int hour = currentTimeUTC.Hour + (int)timeConverter(currentTimeUTC)[0];
+
+
+            // СИНХРОНИЗАЦИЯ СТРЕЛОК
+
+            // Поворот секундной стрелки на 6 градусов за одну секунду
+            float angleSecond = second * 6;
+
+            // Поворот минутной стрелки на 6 градусов в минуту + доворот секундной стрелки
+            float angleMinute = minute * 6 + angleSecond / 60;
+
+            // Поворот часовой стрелки на 30 градусов в час + доворот минутной стрелки
+            float angleHour = hour * 30 + angleMinute / 12;
+
+
+            dialBox.Image = dialImg;
+            dialBox.Controls.Add(hrBox);
+            hrBox.Location = new Point(0, 0);
+            hrBox.Image = rotation(hourImg, angleHour);
+
+            hrBox.Controls.Add(minBox);
+            minBox.Location = new Point(0, 0);
+            minBox.Image = rotation(minuteImg, angleMinute);
+
+            minBox.Controls.Add(secBox);
+            secBox.Location = new Point(0, 0);
+            secBox.Image = rotation(secondImg, angleSecond);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = TimeZoneInfo.GetSystemTimeZones();
+           
+        }
+
+        private Bitmap rotation(Bitmap img, float angle)
+        {
+
+            Bitmap rotatedImage = new Bitmap(img.Width, img.Height);
+            g = Graphics.FromImage(rotatedImage);
+            g.TranslateTransform(rotatedImage.Width / 2, rotatedImage.Height / 2); // Левый верхний угол изображения в центре формы
+            g.RotateTransform(angle); // Угол поворота
+            g.TranslateTransform(-rotatedImage.Width / 2, -rotatedImage.Height / 2); // Центр изображения в центре формы
+            g.DrawImage(img, new Point(0, 0));
+
+            return rotatedImage;
+
+        }
+
+        private int[] timeConverter(DateTime currentTimeUTC)
+        {
+            TimeZoneInfo info = comboBox1.SelectedItem as TimeZoneInfo;
+            int[] newTime = new int[3];
+
+                TimeZoneInfo selectedTimeZone = TimeZoneInfo.FindSystemTimeZoneById(info.Id);
+                TimeSpan offset = selectedTimeZone.GetUtcOffset(currentTimeUTC);
+                newTime[0] = offset.Hours;
+                newTime[1] = offset.Minutes;
+                newTime[2] = offset.Seconds;
+       
+           
+
+            return newTime;
+        }
+    }
+}
